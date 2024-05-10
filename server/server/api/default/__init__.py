b214620546ca9b3ab742cea64a1fd51e60e9121c
Konrad Bryłowski <59873325+kobryl@users.noreply.github.com>
2024-05-10 14:46:38 +0200

    code cleanup

--- server/server/api/default/__init__.py
+++ server/server/api/default/__init__.py
@@ -10,6 +10,9 @@ def start_ws_server():
     websocket.start()
 
 
-if os.environ.get('FLASK_DEBUG') == '0' or not os.environ.get('FLASK_DEBUG') or (os.environ.get('FLASK_DEBUG') == '1' and os.environ.get('WERKZEUG_RUN_MAIN') == 'true'):
+if (os.environ.get('FLASK_DEBUG') == '0'
+        or not os.environ.get('FLASK_DEBUG')
+        or (os.environ.get('FLASK_DEBUG') == '1'
+            and os.environ.get('WERKZEUG_RUN_MAIN') == 'true')):
     ws_thread = threading.Thread(target=websocket.start)
     ws_thread.start()
