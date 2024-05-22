d2ba6418e139ef1d7641dc7b793b996e9cd54518
lukrzak <lukrzak@wp.pl>
2024-05-22 10:53:39 +0200

    Frontend update

--- server/server/api/frontend/views.py
+++ server/server/api/frontend/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from server.db.dataops.map import get_all_maps
-from server.db.dataops.frame import get_latest_frame_from_vehicle
+from server.db.dataops.frame import get_latest_frame_by_ids
 from server.db.dataops.vehicle import get_vehicle
 
 frontend_blueprint = Blueprint('frontend_blueprint', __name__)
@@ -21,7 +21,7 @@ def instance(simulation_id: str, vehicle_id: str):
 
     title = "Simulation " + simulation_id
     vehicle = get_vehicle(int(simulation_id), int(vehicle_id))
-    #camera_views = get_latest_frame_from_vehicle(vehicle)
+    #camera_views = [get_latest_frame_by_ids(int(simulation_id), int(vehicle_id))]
     camera_views = []
 
     return render_template(template_file_path, title=title, camera_views=camera_views, mapId=simulation_id, vehicleId=vehicle_id)
