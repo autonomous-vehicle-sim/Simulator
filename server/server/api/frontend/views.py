a1927bc35c9588515ab834c2a87da17d00317d8b
lukrzak <lukrzak@wp.pl>
2024-05-24 23:23:41 +0200

    Changed static links, to dynamic based on chosen vehicle

--- server/server/api/frontend/views.py
+++ server/server/api/frontend/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from server.db.dataops.map import get_all_maps
+from server.db.dataops.vehicle import get_vehicles
 
 frontend_blueprint = Blueprint('frontend_blueprint', __name__)
 
@@ -9,8 +10,9 @@ def simulations():
     template_file_path = "menu.html"
 
     simulations = get_all_maps()
+    vehicles_per_map = [get_vehicles(sim.id) for sim in simulations]
 
-    return render_template(template_file_path, simulations=simulations)
+    return render_template(template_file_path, simulations=simulations, vehicles=vehicles_per_map)
 
 
 @frontend_blueprint.route('/simulations/<int:simulation_id>/<int:vehicle_id>')
